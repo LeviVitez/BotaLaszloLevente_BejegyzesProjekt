20052257e63af6af329f27f27fce1158d87daeb8
Bóta László Levente <bmwyolo555@gmail.com>
2022-09-29 08:16:22 +0200

    initial commit

--- src/Bejegyzes.java
+++ src/Bejegyzes.java
@@ -1,5 +1,41 @@
-public class Bejegyzes {
-    public static void main(String[] args) {
+import java.time.LocalDateTime;
 
+public class Bejegyzes {
+    private String szerzo;
+    private String tartalom;
+    private int likeok;
+    private LocalDateTime letrejott;
+    private LocalDateTime szerkesztve;
+
+    public Bejegyzes(String szerzo, String tartalom, int likeok, LocalDateTime letrejott, LocalDateTime szerkesztve) {
+        this.szerzo = szerzo;
+        this.tartalom = tartalom;
+        this.likeok = likeok;
+        this.letrejott = letrejott;
+        this.szerkesztve = szerkesztve;
+    }
+
+    public String getSzerzo() {
+        return szerzo;
+    }
+
+    public String getTartalom() {
+        return tartalom;
+    }
+
+    public void setTartalom(String tartalom) {
+        this.tartalom = tartalom;
+    }
+
+    public int getLikeok() {
+        return likeok;
+    }
+
+    public LocalDateTime getLetrejott() {
+        return letrejott;
+    }
+
+    public LocalDateTime getSzerkesztve() {
+        return szerkesztve;
     }
 }
